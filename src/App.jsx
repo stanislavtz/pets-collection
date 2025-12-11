c6180557a2a99a86b8aa6ce3e4e9a5e08b6b6f22
Tzekov <stanislav.tzekov@dxc.com>
2025-12-11 13:34:56 +0200

    Fix styles bug

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Register from "./components/Register";
 import MyPets from "./components/MyPets";
 import CreateEditPet from "./components/CreateEditPet";
 import PetDetails from "./components/PetDetails";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
 
